f603f154b6fd959096a1794acbedd3e339a7ce76
DanielFLima <dan2204@gmail.com>
2021-06-05 13:55:32 -0300

    add Goal feature

---
+++ database/migrations/1622753384157_goal_schema.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class GoalSchema extends Schema {
+  up () {
+    this.create('goals', (table) => {
+      table.increments()
+      
+      table.integer('user_id').unsigned().notNullable();
+      table.foreign('user_id').references('id').inTable('users').onDelete('CASCADE');
+
+      table.integer('category_id').unsigned().notNullable();
+      table.foreign('category_id').references('id').inTable('categories').onDelete('CASCADE');
+
+      table.string('description',90).notNullable()
+      table.float('value_start').notNullable()
+      table.float('value_end').notNullable()
+      table.date('date_start').notNullable()
+      table.date('date_end').notNullable()
+ 
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('goals')
+  }
+}
+
+module.exports = GoalSchema
